7bcd75fb86c19992344c50923c2b71a6d010a7f3
coolcute <coolcute@8e7f2bb0-d97f-11de-8314-133c20781edd>
2009-12-05 01:52:41 +0000

    finished

--- Gui/IndexCell.cpp
+++ Gui/IndexCell.cpp
@@ -27,7 +27,40 @@ IndexCell::IndexCells IndexCell::Load( QStringList strLs, Transition::Transition
 
 	foreach (Transition tran, trans)
 	{
-		ret[tran.targetId].parentId = tran.srcId; 
+		if (tran.Type == "external")
+			ret[tran.targetId].parentId = tran.srcId; 
+	}
+
+	return ret;
+}
+
+IndexCell::IndexCells IndexCell::Load( QStringList strLs, 
+																			Transition::Transitions trans, 
+																			State::States states )
+{
+	assert (strLs.size() == 1);
+	IndexCells ret = Load(strLs, trans);
+	assert (ret.size() == 1);
+	ret.begin().value().states = states;
+	return ret;
+}
+
+IndexCell::State::States IndexCell::State::Load( QStringList strLs )
+{
+	IndexCell::State::States ret;
+
+	foreach (QString str, strLs)
+	{
+		QStringList sls = str.split(regexp);
+		qDebug()<<sls;
+
+		IndexCell::State st;
+
+		st.Id = sls[1].toInt();
+		st.Type= sls[2];
+		st.Name = sls[3];
+
+		ret.push_back(st);
 	}
 
 	return ret;
