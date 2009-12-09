be3e65a4fc615b77582c46b5e466f45edb3a7a54
angelucci.louis <angelucci.louis@8e7f2bb0-d97f-11de-8314-133c20781edd>
2009-12-09 16:54:24 +0000

    Cleaned up class quite a bit and added code to full fill custom IC cell generation requirement.

--- cs2310/ICBuilder/src/com/pitt/softengr/icbuilder/util/fsm/Transform.java
+++ cs2310/ICBuilder/src/com/pitt/softengr/icbuilder/util/fsm/Transform.java
@@ -2,6 +2,8 @@ package com.pitt.softengr.icbuilder.util.fsm;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 
 import com.pitt.softengr.icbuilder.model.fsm.ICSystem;
 import com.pitt.softengr.icbuilder.model.fsm.IndexCell;
@@ -9,11 +11,14 @@ import com.pitt.softengr.icbuilder.model.fsm.State;
 import com.pitt.softengr.icbuilder.model.fsm.StateTransition;
 import com.pitt.softengr.icbuilder.model.fsm.CellTransition;
 import com.pitt.softengr.icbuilder.model.fsm.Message;
+import com.pitt.softengr.icbuilder.model.fsm.Parameter;
 import com.pitt.softengr.icbuilder.model.ic.IcCard;
 import com.pitt.softengr.icbuilder.model.ic.IcCardEntry;
 import com.pitt.softengr.icbuilder.model.ic.IcCardEntryList;
 import com.pitt.softengr.icbuilder.util.fsm.ICSystemModes;
-import com.pitt.softengr.icbuilder.util.ic.StateType;
+import com.pitt.softengr.icbuilder.util.fsm.StateType;
+import com.pitt.softengr.icbuilder.util.fsm.StateTransitionType;
+import com.pitt.softengr.icbuilder.util.fsm.CellTransitionType;
 
 /**
  * Currently this class has one purpose.  Which is to take that resulting merging of the icdb and interactions xml file
@@ -43,13 +48,18 @@ public class Transform {
         //list of all cards 
         List<IcCard> cards = new ArrayList<IcCard>();
         
+        //list of all entries
+        List<IcCardEntry> entries = new ArrayList<IcCardEntry>();
+        
+        //grab all entries
+        entries.addAll(cardSystem.getIcEntryList());
+        
         //grab all the cards
         for(IcCardEntry entry : cardSystem.getIcEntryList()){
             cards.addAll(entry.getIcCardList());
         }
-        
+                
         // if no default cards for index cells are set assume that the inputed cardSystem is one index cell
-        
         if(mode.toLowerCase().equals(ICSystemModes.GROSS.toString().toLowerCase())){
             // make it all one system
             IcCardEntry rootEntry = cardSystem.getRootEntry();
@@ -66,9 +76,9 @@ public class Transform {
 
  //           List<IcCardEntry> entries = cardSystem.getIcEntryList();
             // now create the states
-            List<State> states = buildStates(cards);
+            List<State> states = buildStates(entries);
             // now create the transitions
-            List<StateTransition> transistions = buildStateTransitions(cards,states);
+            List<StateTransition> transistions = buildStateTransitions(entries,states);
             
             cell.addStates(states);
             //set the start state and make it the current state
@@ -86,7 +96,6 @@ public class Transform {
         } else if(mode.toLowerCase().equals(ICSystemModes.FINE.toString().toLowerCase())){
             // make it all one system
             IcCardEntry rootEntry = cardSystem.getRootEntry();
-            List<IcCardEntry> entries = cardSystem.getIcEntryList();
             
             system = new ICSystem();
             system.setID(rootEntry.getEntryId());
@@ -96,120 +105,85 @@ public class Transform {
             List<IndexCell> cells = new ArrayList<IndexCell>();
             
             for(IcCardEntry entry : entries){
-                for(IcCard card : entry.getIcCardList()){
-                    IndexCell cell = new IndexCell();
-                    cell.setID(card.getID());
-                    cell.setMaxLifeTime("infinity");
-                    cell.setName(card.getIcName());
-                    cell.setCurrentState(null);
-                    cell.setStartState(null);
-                    cells.add(cell);
-                }
+                IndexCell cell = new IndexCell();
+                cell.setID(entry.getEntryId());
+                cell.setName(entry.getEntryName());
+                cell.setMaxLifeTime("infinity");
+                cell.setCurrentState(null);
+                cell.setStartState(null);
+                cells.add(cell);
+                
             }
             system.addAllCell(cells);
             
-            List<CellTransition> transitions = buildCellTransitions(cards,cells);
+            List<CellTransition> transitions = buildCellTransitions(entries,cells);
             system.addAllTransitions(transitions);
-            
-            
         } else if(mode.toLowerCase().equals(ICSystemModes.CUSTOM.toString().toLowerCase())){
-            IcCardEntry rootEntry = cardSystem.getRootEntry();
-            List<IcCardEntry> entries = cardSystem.getIcEntryList();
-            
+            IcCardEntry rootEntry = cardSystem.getRootEntry();            
+
             system = new ICSystem();    
             system.setID(rootEntry.getEntryId());
             system.setName(rootEntry.getEntryName());
             system.setDescription("n/a");
-
+            
             List<String> icIDs = cardSystem.getAllIndexIds("1");
             List<IndexCell> cells = new ArrayList<IndexCell>();
-            System.out.println(icIDs.size());
             for(String cellId : icIDs){
-                System.out.println(cellId);
                 IndexCell cell = new IndexCell();
-                cell.setID(cellId);
-                cell.setMaxLifeTime("infinity");
-                List<IcCard> stateCards = new ArrayList<IcCard>();
+                Set<IcCardEntry> temp = new HashSet<IcCardEntry>();
                 for(IcCard card : cards){
                     if(cellId.toLowerCase().equals(card.getGroup().toLowerCase())){
-                        stateCards.add(card);
+                        IcCardEntry entry = cardSystem.findEntryCardByCardID(card.getID());
+                        temp.add(entry);
                     }
                 }
-                List<State> states = buildStates(stateCards);
-                List<StateTransition> trans = buildStateTransitions(stateCards,states);
+                List<IcCardEntry> stateEntries = new ArrayList<IcCardEntry>(temp);    
+                
+                List<State> states = buildStates(stateEntries);
+                if(states.size()>1){
+                    List<StateTransition> stateTransitions = buildStateTransitions(stateEntries,states);
+                    cell.addAllTransitions(stateTransitions);
+                }            
                 cell.addStates(states);
-                cell.addAllTransitions(trans);
+                cell.setMaxLifeTime("infinity");
+//                cell.setStartState(null);
+                cell.setCurrentState(null);
+                
+//                cell.setID(cell.getStartState().getID());
+//                cell.setName(cell.getStartState().getName());
+                cell.setID(cellId);
                 cells.add(cell);
-                System.out.println(cells);
             }
+            List<CellTransition> cellTransitions = buildCellTransitions(entries,cells);
+            system.addAllTransitions(cellTransitions);
             system.addAllCell(cells);
-            List<CellTransition> transitions = buildCellTransitions(cards,cells);
-            System.out.println(transitions);
-            system.addAllTransitions(transitions);
         } else {
             throw new RuntimeException("UNKNOW SYSTEM SCHEME \""+mode+"\" .... PLEASE CHECK CONFIGURATION FILE!!! ... ABORTING.");
         }
         return system;
     }
-    private void buildSystems(){
-        
-    }
-    private void buildSystem(){
-        
-    }
-    private void buildCells(){
-        
-    }
-    private void buildCell(IcCard card){
-    }
- /*   private List<State> buildStates(List<IcCardEntry> entries){
+    private List<State> buildStates(List<IcCardEntry> stateEntries){
         List<State> states = new ArrayList<State>();
-        for(IcCardEntry entry : entries){
-            for(IcCard card : entry.getIcCardList()){
-                State state = buildState(card);
-                if(state!=null){
-                    states.add(state);
-                }
-            }
-        }
-        return states;
-    }
- */
-    private List<State> buildStates(List<IcCard> cards){
-        List<State> states = new ArrayList<State>();
-        for(IcCard card : cards){
-            State state = buildState(card);
+        for(IcCardEntry entry : stateEntries){
+            State state = buildState(entry);
             if(state!=null){
                 states.add(state);
             }
         }
         return states;
     }
-    private State buildState(IcCard card){
-        State state = null;
-        if(card.isEndCard()){
-            state = new State();
-            state.setID(card.getID());
-            state.setName(card.getIcName());
-            state.setType(StateType.ENDING);
-        } else if(card.isStartCard()){
-            state = new State();
-            state.setID(card.getID());
-            state.setName(card.getIcName());
-            state.setType(StateType.ENTERING);
-        } else {
-            state = new State();
-            state.setID(card.getID());
-            state.setName(card.getIcName());
-            state.setType(StateType.INTERNAL);
-        }
+    private State buildState(IcCardEntry entryCard){
+        State state = new State();
+        state.setID(entryCard.getEntryId());
+        state.setName(entryCard.getEntryName());
+        state.setType(StateType.internal);
         return state;
     }
-/*    private List<CellTransition> buildCellTransitions(List<IcCardEntry> entries, List<IndexCell> cells){
+    private List<CellTransition> buildCellTransitions(List<IcCardEntry> entries, List<IndexCell> cells){
         List<CellTransition> transitions = new ArrayList<CellTransition>();
+        int countTrans=0;
+        int countMsg=0;
         for(IcCardEntry entry : entries){
-            int countTrans=0;
-            int countMsg=0;
             for(IcCard card : entry.getIcCardList()){
                 if(card.getIcOtherId()!=null && !card.getIcOtherId().isEmpty()){
                     if(Integer.parseInt(card.getIcOtherId())>0){
@@ -219,15 +193,17 @@ public class Transform {
                             source = findCellById(card.getID(),cells);
                         } else {
                             source = findCellById(card.getIcParentId(),cells);
-                        }
+                        }                       
+                        
                         trans.setSourceCell(source);
+                       
                         Message msg = new Message();
-                        msg.setID("msg"+countMsg);
+                        msg.setID(""+countMsg);
                         trans.setID(""+countTrans);
-                     
+  
                         IndexCell target = findCellById(card.getIcOtherId(),cells);
                         trans.setTargetCell(target);
-                        trans.setTransitionType("internal");
+                        trans.setTransitionType(CellTransitionType.internal);
                         msg.setName(card.getIcOtherTask());
                         msg.setMessageString(card.getIcOtherMessage());
                         trans.setTransitionMessage(msg);
@@ -240,126 +216,100 @@ public class Transform {
         }
         return transitions;
     }
-*/    
-    private List<CellTransition> buildCellTransitions(List<IcCard> cards, List<IndexCell> cells){
-        List<CellTransition> transitions = new ArrayList<CellTransition>();
-        int countTrans=0;
-        int countMsg=0;
-        for(IcCard card : cards){
-            if(card.getIcOtherId()!=null && !card.getIcOtherId().isEmpty()){
-                if(Integer.parseInt(card.getIcOtherId())>0){
-                    CellTransition trans = new CellTransition();
-                    IndexCell source = null;
-                    if(card.getIcParentId().equals("-1")){
-                        source = findCellById(card.getID(),cells);
-                    } else {
-                        source = findCellById(card.getIcParentId(),cells);
-                    }
-                    trans.setSourceCell(source);
-                    Message msg = new Message();
-                    msg.setID("msg"+countMsg);
-                    trans.setID(""+countTrans);
-                 
-                    IndexCell target = findCellById(card.getIcOtherId(),cells);
-                    trans.setTargetCell(target);
-                    trans.setTransitionType("internal");
-                    msg.setName(card.getIcOtherTask());
-                    msg.setMessageString(card.getIcOtherMessage());
-                    trans.setTransitionMessage(msg);
-                    transitions.add(trans);
-                    countTrans++;
-                    countMsg++;
-                }
-            }
-        }
-        return transitions;
-    }
-
-/*    private List<StateTransition> buildStateTransitions(List<IcCardEntry> entries, List<State> states){
+    private List<StateTransition> buildStateTransitions(List<IcCardEntry> stateEntries, List<State> states){
         //the return list
         List<StateTransition> transitions = new ArrayList<StateTransition>();
-        //now build all the transitions
-        for(IcCardEntry entry : entries){
+        List<StateTransition> tempList = new ArrayList<StateTransition>();
+        for(IcCardEntry entry : stateEntries){
             int countTrans=0;
             int countMsg=0;
             for(IcCard card : entry.getIcCardList()){
-                if(card.getIcOtherId()!=null && !card.getIcOtherId().isEmpty()){
-                    //if this is the case transition is present
-                    if(Integer.parseInt(card.getIcOtherId())>0){
+                if(!card.getIcOtherId().isEmpty() && !card.getIcParentId().isEmpty()){
                          StateTransition trans = new StateTransition();
+                         
                          State source = null;
                          if(card.getIcParentId().equals("-1")){
                              source = findStateById(card.getID(),states);
                          } else {
-                             source  = findStateById(card.getIcParentId(),states);
+                             source = findStateById(card.getIcParentId(),states);
                          }
+                             
                          trans.setSourceState(source);
+                         
                          Message msg = new Message();
-                         msg.setID("msg"+countMsg);
+                         msg.setID(""+countMsg);
                          trans.setID(""+countTrans);
-                         State target=findStateById(card.getIcOtherId(),states);
+                               
+                         State target =  null;
+                         if(card.getIcOtherId().equals("-1")){
+                             target=findStateById(card.getID(),states);
+                         } else {
+                             target=findStateById(card.getIcOtherId(),states);
+                         }
                          trans.setTargetState(target);
-                         trans.setTransitionType("internal");
+                         trans.setTransitionType(StateTransitionType.internal);
+                         
                          msg.setName(card.getIcOtherTask());
                          msg.setMessageString(card.getIcOtherMessage());
+                         msg.setType("input");
+                         
+                         Parameter par = new Parameter();
+                         par.setDataValue(card.getTimeCriticalCondition());
+                         par.setDataType("time");
+                         par.setName("Tc");
+                         
+                         msg.addParameter(par);
                          trans.setTransitionMessage(msg);
-                         transitions.add(trans);
+                         
+                         tempList.add(trans);
                          countTrans++;
                          countMsg++;
                     }
-                }
+ //               }
             }
         }
+        for(StateTransition trans : tempList){
+            if(trans.getSourceState()!=null && trans.getTargetState()!=null){
+                transitions.add(trans);
+            }
+       }
         return transitions;
     }
-*/
-    private List<StateTransition> buildStateTransitions(List<IcCard> cards, List<State> states){
-        //the return list
-        List<StateTransition> transitions = new ArrayList<StateTransition>();
-        //now build all the transitions
-        int countTrans=0;
-        int countMsg=0;
-        for(IcCard card : cards){
-            if(card.getIcOtherId()!=null && !card.getIcOtherId().isEmpty()){
-                //if this is the case transition is present
-                if(Integer.parseInt(card.getIcOtherId())>0){
-                     StateTransition trans = new StateTransition();
-                     State source = null;
-                     if(card.getIcParentId().equals("-1")){
-                         source = findStateById(card.getID(),states);
-                     } else {
-                         source  = findStateById(card.getIcParentId(),states);
-                     }
-                     trans.setSourceState(source);
-                     Message msg = new Message();
-                     msg.setID("msg"+countMsg);
-                     trans.setID(""+countTrans);
-                     State target=findStateById(card.getIcOtherId(),states);
-                     trans.setTargetState(target);
-                     trans.setTransitionType("internal");
-                     msg.setName(card.getIcOtherTask());
-                     msg.setMessageString(card.getIcOtherMessage());
-                     trans.setTransitionMessage(msg);
-                     transitions.add(trans);
-                     countTrans++;
-                     countMsg++;
-                }
+    private String findEntryIdByIcCardId(String ID){
+        for(IcCardEntry entry : cardSystem.getIcEntryList()){
+            for(IcCard card : entry.getIcCardList()){
+                if(ID.equals(card.getID())){
+                    return entry.getEntryId();
+                }             
             }
         }
-        return transitions;
+        return null;
     }
     private State findStateById(String ID, List<State> states){
         for(State state : states){
-            if(state.getID().toLowerCase().equals(ID.toLowerCase())){
+            if((findEntryIdByIcCardId(ID)!=null) && (state.getID().toLowerCase().equals(findEntryIdByIcCardId(ID).toLowerCase()))){
+                return state;
+            } else if((ID!=null) && (state.getID().toLowerCase().equals(ID.toLowerCase()))){
                 return state;
             }
         }
         return null;
     }
     private IndexCell findCellById(String ID, List<IndexCell> cells){
+ //       System.out.println("ID "+ID+" ID FOUND "+(findEntryIdByIcCardId(ID)));
         for(IndexCell cell : cells){
-            if(cell.getID().toLowerCase().equals(ID.toLowerCase())){
+  //          System.out.println("CELL "+cell.getID());
+            if((findEntryIdByIcCardId(ID)!=null) && (cell.getID().toLowerCase().equals(findEntryIdByIcCardId(ID).toLowerCase()))){
                 return cell;
+            } else if((ID!=null) && (cell.getID().toLowerCase().equals(ID.toLowerCase()))){
+                return cell;
+            }
+            for(State state : cell.getStates()){
+                if((findEntryIdByIcCardId(ID)!=null) && (state.getID().toLowerCase().equals(findEntryIdByIcCardId(ID).toLowerCase()))){
+                    return cell;
+                } else if((ID!=null) && (state.getID().toLowerCase().equals(ID.toLowerCase()))){
+                    return cell;
+                }
             }
         }
         return null;
