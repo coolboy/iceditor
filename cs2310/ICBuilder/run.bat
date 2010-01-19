1eb972f17bc18ef3f2a0673d8ca089a8d8a3781f
angelucci.louis <angelucci.louis@8e7f2bb0-d97f-11de-8314-133c20781edd>
2010-01-19 17:16:12 +0000

    Script to run the jar.

--- cs2310/ICBuilder/run.bat
+++ cs2310/ICBuilder/run.bat
@@ -1,3 +1,2 @@
-java -cp \dist\ICBuilder-0.0.1.jar \lib\log4j-1.2.15.jar -Dicbuilder.conf=./config/config.properties com.pitt.softengr.icbuilder.commandLine
-pause
+java -cp "dist\ICBuilder-1.0.0.jar;lib\log4j-1.2.15.jar;" -Dicbuilder.conf="config\config.properties" com.pitt.softengr.icbuilder.commandLine > run.log
 ECHO Batch Processing Complete
