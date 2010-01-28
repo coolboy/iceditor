26fd99b100d2eeb548cc4e82051f08db9ee3dbb2
coolcute <coolcute@8e7f2bb0-d97f-11de-8314-133c20781edd>
2010-01-28 02:48:16 +0000

    init

---
+++ trunk/cs3210/Gui/cpuplot.hxx
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2010, Sysu
+ * 保留所有权利。( All rights reserved. )
+ * 
+ * 文件名称：cpuplot.hxx
+ * 文件标识：
+ * 摘    要：
+ * 
+ * 当前版本：1.0
+ * 作    者：Coolboy
+ * 完成日期：2010年1月27日
+ *
+ * 取代版本：
+ * 原作者  ：
+ * 完成日期：
+ */
+#pragma once
+#include <qwt_plot.h>
+
+#include "cpustat.h"
+
+static const int HISTORY = 60; // seconds
+
+class QwtPlotCurve;
+
+class CpuPlot : public QwtPlot 
+{
+    Q_OBJECT
+public:
+    enum CpuData
+    {
+        User,
+        System,
+        Total,
+        Idle,
+
+        NCpuData
+    };
+
+    CpuPlot(QWidget * = 0);
+    const QwtPlotCurve *cpuCurve(int id) const
+        { return data[id].curve; }
+
+protected:
+    void timerEvent(QTimerEvent *e);
+
+private Q_SLOTS:
+    void showCurve(QwtPlotItem *, bool on);
+
+private:
+    struct
+    {
+        QwtPlotCurve *curve;
+        double data[HISTORY];
+    } data[NCpuData];
+    double timeData[HISTORY];
+
+    int dataCount;
+    CpuStat cpuStat;
+};
---
+++ trunk/cs3210/Gui/stdafx.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <QtGui/QtGui>
+
+#include <qwt.h>
