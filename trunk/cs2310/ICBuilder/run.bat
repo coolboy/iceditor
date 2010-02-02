aaa579761ce0dd527e944d160c5f6a47c0e565eb
angelucci.louis <angelucci.louis@8e7f2bb0-d97f-11de-8314-133c20781edd>
2010-02-02 05:23:09 +0000

    changed the run log to be generated in the logs directory.

--- trunk/cs2310/ICBuilder/run.bat
+++ trunk/cs2310/ICBuilder/run.bat
@@ -1,2 +1,2 @@
-java -cp "dist\ICBuilder-1.0.0.jar;lib\log4j-1.2.15.jar;" -Dicbuilder.conf="config\config.properties" com.pitt.softengr.icbuilder.commandLine > run.log
+java -cp "dist\ICBuilder-1.0.0.jar;lib\log4j-1.2.15.jar;" -Dicbuilder.conf="config\config.properties" com.pitt.softengr.icbuilder.commandLine > .\logs\run.log
 ECHO Batch Processing Complete
