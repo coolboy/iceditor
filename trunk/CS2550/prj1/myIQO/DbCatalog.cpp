f79784b2ddfa561762ecae192b439d86faa3f945
yong1222 <yong1222@8e7f2bb0-d97f-11de-8314-133c20781edd>
2010-03-23 22:21:33 +0000

    DbCatalog Module finished.

--- trunk/CS2550/prj1/myIQO/DbCatalog.cpp
+++ trunk/CS2550/prj1/myIQO/DbCatalog.cpp
@@ -5,12 +5,19 @@
 DbCatalog::DbCatalog(string dbSchema, string dbIndexing, string dbConfig)
 {
     
-	string delimiters = " \t\n(:->,";
+	string delimiters = " \t\n();:>,";
 	string token;
 	string CurTabName;
 	string attr;
 	string lengh;
+	string index;
+	string IdxBfr;
+	string pre_token;
+	string PageSize;  //is this needed to be stored in the class?
+	string BuffSize;  //is this needed to be stored in the class?
+	Idx_Type IdxType;
 	Atr_Type type;
+
 	/* process the dbSchema string */
 
 	// Skip delimiters at beginning.
@@ -64,8 +71,8 @@ DbCatalog::DbCatalog(string dbSchema, string dbIndexing, string dbConfig)
 		else
 		{
 		    attr = token;
-			lastPos = dbSchema.find_first_not_of(" ,", pos);
-            pos = dbSchema.find_first_of(" ,", lastPos);
+			lastPos = dbSchema.find_first_not_of(" ,\n", pos);
+            pos = dbSchema.find_first_of(" ,\n", lastPos);
 			token = dbSchema.substr(lastPos, pos - lastPos);
 			if(token.compare("char")==0 || token.compare("CHAR")==0)
 			{
@@ -102,11 +109,114 @@ DbCatalog::DbCatalog(string dbSchema, string dbIndexing, string dbConfig)
 
     }
 
+
+
 	/* process the dbIndexing string */
+    lastPos = dbIndexing.find_first_not_of(delimiters, 0);
+    pos = dbIndexing.find_first_of(delimiters, lastPos);
+	while (string::npos != pos || string::npos != lastPos)
+    {
+       
+            CurTabName = dbIndexing.substr(lastPos, pos - lastPos);
+
+		    lastPos = dbIndexing.find_first_not_of(delimiters, pos);
+            pos = dbIndexing.find_first_of(delimiters, lastPos);
+			attr = dbIndexing.substr(lastPos, pos - lastPos);
+
+			lastPos = dbIndexing.find_first_not_of(delimiters, pos);
+            pos = dbIndexing.find_first_of(delimiters, lastPos);
+			index = dbIndexing.substr(lastPos, pos - lastPos);
+
+			if(index.compare("B+-tree") == 0)
+				IdxType = BTREE_T;
+			else if(index.compare("Extensible") == 0)
+			    IdxType = EHASH_T;
+			else if(index.compare("Linear") == 0)
+			    IdxType = LHASH_T;
+			else 
+				IdxType = NONE_T;
+
+			lastPos = dbIndexing.find_first_not_of(delimiters, pos);
+            pos = dbIndexing.find_first_of(delimiters, lastPos);
+			IdxBfr = dbIndexing.substr(lastPos, pos - lastPos);
+
+			if(IdxBfr.compare("Hashing") == 0)
+			{
+			   lastPos = dbIndexing.find_first_not_of(delimiters, pos);
+               pos = dbIndexing.find_first_of(delimiters, lastPos);
+			   IdxBfr = dbIndexing.substr(lastPos, pos - lastPos);
+			}
+
+		    SetIdx(CurTabName, attr, IdxType, atoi(IdxBfr.c_str()));
+
+            lastPos = dbIndexing.find_first_not_of(delimiters, lastPos);  //read next line
+            pos = dbIndexing.find_first_of(delimiters, lastPos);
+
+    }
+
 
 
 	/* process the dbConfig string */
+	lastPos = dbConfig.find_first_not_of(delimiters, 0);
+    pos = dbConfig.find_first_of(delimiters, lastPos);
+	while (string::npos != pos || string::npos != lastPos)
+    {
+        // Found a token, add it to the vector.
+		
+		pre_token = dbConfig.substr(lastPos, pos - lastPos);
+		if(pre_token.compare(0,1,"#") || pre_token.compare("}"))
+		{
+		    lastPos = dbSchema.find_first_of("\n", pos);
+            lastPos = dbSchema.find_first_not_of(delimiters, lastPos);  //read next line
+            pos = dbSchema.find_first_of(delimiters, lastPos);
+		    continue;
+		}
 
+		lastPos = dbConfig.find_first_not_of(delimiters, 0);
+        pos = dbConfig.find_first_of(delimiters, lastPos);
+        token = dbConfig.substr(lastPos, pos - lastPos);
+		lastPos = dbConfig.find_first_not_of(delimiters, 0);
+        pos = dbConfig.find_first_of(delimiters, lastPos);
+
+		if (pre_token.compare("Page") == 0 && token.compare("Size") == 0)
+		{
+		   // read next token(page size)
+		   PageSize = dbConfig.substr(lastPos, pos - lastPos);
+
+           lastPos = dbConfig.find_first_not_of(delimiters, pos);
+           pos = dbConfig.find_first_of(delimiters, lastPos);
+		   
+		   continue;
+           
+		}
+		else if(pre_token.compare("Memory") == 0 && token.compare("Buffer") == 0)
+		{
+		    BuffSize = dbConfig.substr(lastPos, pos - lastPos);
+			lastPos = dbSchema.find_first_of("\n", pos);
+            lastPos = dbSchema.find_first_not_of(delimiters, lastPos);  //read next line
+            pos = dbSchema.find_first_of(delimiters, lastPos);
+		}
+
+		else if(token.compare("{")==0 )
+		{
+			CurTabName = pre_token;
+		}
+
+		else if(pre_token.compare("Cardinality") == 0)
+		{
+			SetCardi(CurTabName,atoi(token.c_str()));
+		}
+
+		else if(pre_token.compare("bfr") == 0)
+		{
+			SetBfr(CurTabName,atoi(token.c_str()));
+		}
+		else if(token.compare(0,1,"0") == 0)
+		{
+			SetSel(CurTabName, pre_token, atof(token.c_str()));
+		}
+
+    }
 
 }
 
@@ -117,7 +227,9 @@ DbCatalog::DbCatalog(void)
 
 int DbCatalog::AddTable(string s)
 {
-   return 0;
+	DbTable NewTable(s);
+	TableList.push_back(NewTable);
+    return 0;
 }
 
 /*
@@ -136,36 +248,78 @@ DbTable* DbCatalog::SearchTable(string tab_name, )
 
 long int DbCatalog::GetCardi(string tab_name)
 {
+   list<DbTable>::iterator tab_iter;
+   for(tab_iter=TableList.begin(); tab_iter!=TableList.end(); ++tab_iter)
+   {
+       if (tab_iter->GetName() == tab_name)
+		   return tab_iter->GetCardi();
+   }
    return 0;
 }
 
 int DbCatalog::GetBfr(string tab_name)
 {
+   list<DbTable>::iterator tab_iter;
+   for(tab_iter=TableList.begin(); tab_iter!=TableList.end(); ++tab_iter)
+   {
+       if (tab_iter->GetName() == tab_name)
+		   return tab_iter->GetBfr();
+   }
    return 0;
 }
 
 string DbCatalog::GetPk(string tab_name)
 {
+   list<DbTable>::iterator tab_iter;
+   for(tab_iter=TableList.begin(); tab_iter!=TableList.end(); ++tab_iter)
+   {
+       if (tab_iter->GetName() == tab_name)
+		   return tab_iter->GetPk();
+   }
    return " ";
 }
 
 int DbCatalog::IsPk(string tab_name, string attr_name)
 {
-   return 0;
+   list<DbTable>::iterator tab_iter;
+   for(tab_iter=TableList.begin(); tab_iter!=TableList.end(); ++tab_iter)
+   {
+       if (tab_iter->GetName() == tab_name)
+		   return tab_iter->IsFk(attr_name);
+   }
+   return -1;
 }
 
 double DbCatalog::GetSel(string tab_name, string attr_name)
 {
+   list<DbTable>::iterator tab_iter;
+   for(tab_iter=TableList.begin(); tab_iter!=TableList.end(); ++tab_iter)
+   {
+       if (tab_iter->GetName() == tab_name)
+		   return tab_iter->GetSel(attr_name);
+   }
    return 0;
 }
 
 Idx_Type DbCatalog::GetIdx(string tab_name, string attr_name)
 {
+   list<DbTable>::iterator tab_iter;
+   for(tab_iter=TableList.begin(); tab_iter!=TableList.end(); ++tab_iter)
+   {
+       if (tab_iter->GetName() == tab_name)
+		   return tab_iter->GetIdx(attr_name);
+   }
    return NONE_T;
 }
 
 int DbCatalog::GetIdxBfr(string tab_name, string attr_name)
 {
+   list<DbTable>::iterator tab_iter;
+   for(tab_iter=TableList.begin(); tab_iter!=TableList.end(); ++tab_iter)
+   {
+       if (tab_iter->GetName() == tab_name)
+		   return tab_iter->GetIdxBfr(attr_name);
+   }
    return 0;
 }
 
