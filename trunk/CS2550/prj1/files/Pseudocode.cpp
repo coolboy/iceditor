3fd207b9129e8a11d192efe4be8b52b44c7fad18
coolcute <coolcute@8e7f2bb0-d97f-11de-8314-133c20781edd>
2010-03-30 01:19:04 +0000

    policy's pseudo code

--- trunk/CS2550/prj1/files/Pseudocode.cpp
+++ trunk/CS2550/prj1/files/Pseudocode.cpp
@@ -1,33 +1,36 @@
 /*  Pseudocode for SQL Optimizer  */
 
 // step 1(step1,2,3 in the slides):  
-
+/* Yang's job */
 foreach node in the querytree
 {
   if (node.type="select")
   	{
   		 if(condition A='B'||A=NUMBER||A<NUMBER||A>NUMBER)
   		   search all children whose type is scan[table], and find
-  	     all the attributes in the table associated with the scan.
-  	     if there is one attribute match A,
-  	     then, remove this select node such as it becomes
-  	     the scan[table]\'s direct parent.   
+  	     all the fields in the table associated with the scan.
+		 
+  	     if there is one field match A,
+  	     then, move this select node and make it becomes
+  	     the scan[table]\'s direct parent.
   		 
-  		 else if(condition A.B='C'||....)  
+  		 else if(condition T.B='C'||....)  
   		 	 search all children whose type is scan[table], 
-  	     if table matches A,  remove this select node such as 
+  	     if table matches T,  remove this select node such as 
   	     it becomes the scan[table]\'s direct parent.
   		 
-  		 else if(condition A=B)
+  		 else if(condition A = B) //T1 and T2 omitted
   		    search all children whose type is PRODUCT,
   		    for every searched PRODUCT, find its left and
   		    right child, if left and right child(or its
-  		    	child, child\'child)contains A and B, then remove
-  		    	the select such that it is the parent of this PRODUCT,
-  		    	if left child has no A or B, go to its right child
+  		    	child, child\'child)contains A and B, then move
+  		    	the select and make it is the parent of this PRODUCT,
+				
+  		    	if left child doesn't have A or B, go to its right child
   		    		and continue until find a PRODUCT such that
   		    		the PRODUCT\'s left and right child contain
-  		    		A and B respectively 
+  		    		A and B respectively
+				
   		    	if right child has no A or B, go to left and do the 
   		    		similar thing
   		    remove the found PRODUCT , add JOIN with condition A=B 
@@ -43,43 +46,51 @@ foreach node in the querytree
   		 	    and do the similar things above
   		 	    
   		 else if (condition has an OR)
-  		 	  	
   		 	  	do nothing
   		 	  	
   		 else if (condition has an NOT)
-  		    
   		      do similar as first and second condition, but
   		      use different cost famular? 
   	}
 }
 
 /* step 2(step 4 in the slide): choose the join order and method*/
+/* Yong's job */
+/*
+Join selectivity:
+|(R JOIN S)| / (|R| * |S|)
+Assume 
+If A is a key of R, then |(R Join S)| <= |S|, so js <= (1/|R|).
 
-for each node
-{
-	 choose join order based on js , how?  some child has select sub-child should be first  //there is an alternative: page543
+R.A is unique so that R Join S will be max as S, because 
+like 
+R.A  S.B
+123  123
+456  123
+789  456
+
+the joined table will be 3, the worst case is that every S.B can be found in R.A
+where R.A is unque so that the most is S.B * 1.
+*/
+for each node {
+	 choose join order based on join selectivity, how?  some child has select sub-child should be first  //there is an alternative: page543
 	 choose algorithm based on index available, whether sorted or not 
 }
 
 /* step 3(step 5 in the slide): put down the project node as low as possible 
 while keeping the relevant fields */
-
-for each node 
-{
-	 if(node.type = SCAN)
-	 	  retrive the table in the SCAN, and traverse the querytree backwards to find all 
-	 	  attribute of the table involved in all ancestors of this SCAN node. 
-	 	  these attributes form a list L. Add project(L) node above the SCAN node as its parent. 
-	 	 
+/* Yang's job */
+for each node {
+	 if(node.type == SCAN)
+	 	  retrive the table name in the SCAN, and traverse the querytree backwards to find all 
+	 	  fields of this table that be used in all of his ancestor node.
+		  
+	 	  These fields form a list L. Add project(L) node above the SCAN node as its parent.
 }
 
 /* step 4(calculate the cost) */
-    
-    
-   calculate the cost, using formulas on page 536(for select, project can be also
-   calculated?),  539(for join), 509(for union, need to figure out details) and 
-   542/543(materialized cost, namely, how the execution of first node effects the 
-   cost of subsequent nodes.)
-   
-   
-   
+/* Yongg's job */
+calculate the cost, using formulas on page 536(for select, project can be also
+calculated?),  539(for join), 509(for union, need to figure out details) and 
+542/543( materialized cost, namely, how the execution of first node effects the 
+cost of subsequent nodes.)
