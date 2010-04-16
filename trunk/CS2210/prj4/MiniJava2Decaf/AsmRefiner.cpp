a3f169dfdbb843e2966d10dd1cd29a3f2e8acd18
coolcute <coolcute@8e7f2bb0-d97f-11de-8314-133c20781edd>
2010-04-16 03:53:18 +0000

    init

---
+++ trunk/CS2210/prj4/MiniJava2Decaf/AsmRefiner.cpp
@@ -0,0 +1,53 @@
+#include "StdAfx.h"
+#include "asmrefiner.h"
+
+
+AsmRefiner::AsmRefiner(void)
+{
+}
+
+
+AsmRefiner::~AsmRefiner(void)
+{
+}
+
+void AsmRefiner::setDecafAsm( const std::string& dasm )
+{
+	decafAsm_ = dasm;
+}
+
+std::string AsmRefiner::getMiniJavaAsm()
+{
+	transform();
+	return miniJavaAsm_;
+}
+
+void AsmRefiner::transform()
+{
+	/* headr refine
+	* - # standard Decaf preamble //maybe delete this line in dcc source?
+	* + .data #pre df str for "\n" //this for the predefined "\n"
+	* + Enter:	.asciiz "
+	* + "
+	*/
+
+	/* Replace PrintInt
+	* -  jal _PrintInt      	# jump to function
+	* + li	$v0	1			#choice print_int syscall
+	* + lw	$a0	4($sp)		#integer to print
+	* + syscall				#print the arg
+	* + li	$v0	4		#print_str
+	* + la	$a0	Enter		#address of "\n" to print
+	* + syscall				#print the arg
+	*/
+
+	/* Replace PrintString
+	* -  jal _PrintString      	# jump to function
+	* + li	$v0	4			#print_str
+	* + la	$a0	($t0)		#address of entering to print
+	* + syscall				#print the arg
+	* + li	$v0	4		#print_str
+	* + la	$a0	Enter		#address of "\n" to print
+	* + syscall				#print the arg
+	*/
+}
---
+++ trunk/CS2210/prj4/MiniJava2Decaf/MiniJava2Decaf.cpp
@@ -0,0 +1,59 @@
+#include "StdAfx.h"
+#include "minijava2decaf.h"
+
+
+MiniJava2Decaf::MiniJava2Decaf(void)
+{
+}
+
+
+MiniJava2Decaf::~MiniJava2Decaf(void)
+{
+}
+
+void MiniJava2Decaf::setMiniJava( const std::string& miniJava )
+{
+	miniJava_ = miniJava;
+}
+
+std::string MiniJava2Decaf::getDecaf()
+{
+	transform ();
+
+	return decaf_;
+}
+
+void MiniJava2Decaf::transform()
+{
+	/*strip the comment
+	 * 1. / *...* /
+	 * 2. //...
+	*/
+
+	/*
+	 * strip program keyword
+	 * 
+	*/
+
+	/*
+	* strip declarations/enddeclarations keyword
+	* declarations
+	* int x=-1;
+	* enddeclarations
+	*/
+
+	/*
+	* strip declarations/enddeclarations keyword
+	* declarations
+	* int x=-1;
+	* enddeclarations
+	*/
+
+	/*
+	* Move main function out and append to the file end.
+	*/
+
+	/*
+	* Change Print to System.println
+	*/
+}
