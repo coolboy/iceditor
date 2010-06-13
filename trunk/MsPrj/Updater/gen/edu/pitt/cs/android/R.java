de97cbc7dbd2c03add35360c6124ab82e4b5f173
coolcute <coolcute@8e7f2bb0-d97f-11de-8314-133c20781edd>
2010-06-13 20:46:21 +0000

    Gui Framework Done!!

--- trunk/MsPrj/Updater/gen/edu/pitt/cs/android/R.java
+++ trunk/MsPrj/Updater/gen/edu/pitt/cs/android/R.java
@@ -16,26 +16,29 @@ public final class R {
         public static final int icon=0x7f020002;
     }
     public static final class id {
-        public static final int file_manager=0x7f050001;
+        public static final int diff_button=0x7f050005;
         public static final int file_path1=0x7f050000;
-        public static final int file_path2=0x7f050002;
-        public static final int file_path_out=0x7f050003;
-        public static final int open=0x7f050004;
-        public static final int pick_directory=0x7f050006;
-        public static final int save=0x7f050005;
+        public static final int file_path2=0x7f050001;
+        public static final int file_path_out=0x7f050002;
+        public static final int merge_button=0x7f050006;
+        public static final int open=0x7f050003;
+        public static final int save=0x7f050004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
+        public static final int apk_file_1=0x7f040008;
+        public static final int apk_file_2=0x7f040009;
         public static final int app_name=0x7f040000;
-        public static final int file_name=0x7f040002;
+        public static final int diff_button_text=0x7f040006;
+        public static final int merge_button_text=0x7f04000b;
         public static final int no_filemanager_installed=0x7f040001;
-        public static final int open_button=0x7f040004;
-        public static final int open_title=0x7f040003;
-        public static final int pick_directory_button=0x7f040008;
-        public static final int pick_directory_title=0x7f040007;
-        public static final int save_button=0x7f040006;
-        public static final int save_title=0x7f040005;
+        public static final int open_button=0x7f040003;
+        public static final int open_title=0x7f040002;
+        public static final int out_file=0x7f04000a;
+        public static final int pick_directory_button=0x7f040007;
+        public static final int save_button=0x7f040005;
+        public static final int save_title=0x7f040004;
     }
 }
