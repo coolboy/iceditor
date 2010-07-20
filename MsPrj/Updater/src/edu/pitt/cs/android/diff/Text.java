3ff7d67c673f215e6ec1b91e4ec66e1ce5224f78
coolcute <coolcute@8e7f2bb0-d97f-11de-8314-133c20781edd>
2010-07-20 04:12:21 +0000

    Finished the diff framework

---
+++ MsPrj/Updater/src/edu/pitt/cs/android/diff/Text.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package edu.pitt.cs.android.diff;
+
+/**
+ * @author Cool
+ *
+ */
+public class Text implements IDiff {
+
+	/* (non-Javadoc)
+	 * @see edu.pitt.cs.android.diff.IDiff#diff(byte[], byte[])
+	 */
+	public byte[] diff(byte[] originData, byte[] newData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.pitt.cs.android.diff.IDiff#merge(byte[], byte[])
+	 */
+	public byte[] merge(byte[] originData, byte[] diffData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
