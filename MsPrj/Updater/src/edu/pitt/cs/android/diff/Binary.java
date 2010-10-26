5bdb7bc700c76aebc24e693c60668f59492e0e00
coolcute <coolcute@8e7f2bb0-d97f-11de-8314-133c20781edd>
2010-10-26 01:55:16 +0000

    xdelta ready

--- MsPrj/Updater/src/edu/pitt/cs/android/diff/Binary.java
+++ MsPrj/Updater/src/edu/pitt/cs/android/diff/Binary.java
@@ -3,6 +3,15 @@
  */
 package edu.pitt.cs.android.diff;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+
+import com.nothome.delta.Delta;
+import com.nothome.delta.DiffWriter;
+import com.nothome.delta.GDiffPatcher;
+import com.nothome.delta.GDiffWriter;
+
 /**
  * @author Cool
  *
@@ -12,17 +21,26 @@ public class Binary implements IDiff {
 	/* (non-Javadoc)
 	 * @see edu.pitt.cs.android.diff.IDiff#diff(byte[], byte[])
 	 */
-	public byte[] diff(byte[] originData, byte[] newData) {
-		//TODO
-		return newData;
+	public byte[] diff(byte[] originData, byte[] newData) throws Exception {
+		
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        DiffWriter diffWriter = new GDiffWriter(new DataOutputStream(outputStream));
+		Delta delta = new Delta();
+		delta.compute(originData, new ByteArrayInputStream(newData), diffWriter);
+        diffWriter.close();
+            
+        return outputStream.toByteArray();
 	}
 
 	/* (non-Javadoc)
 	 * @see edu.pitt.cs.android.diff.IDiff#merge(byte[], byte[])
 	 */
-	public byte[] merge(byte[] originData, byte[] diffData) {
-		// TODO Auto-generated method stub
-		return diffData;
+	public byte[] merge(byte[] originData, byte[] diffData) throws Exception {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		GDiffPatcher diffPatcher = new GDiffPatcher();
+		diffPatcher.patch(originData, new ByteArrayInputStream(diffData), outputStream);
+		
+        return outputStream.toByteArray();
 	}
 
 }
--- MsPrj/Updater/src/edu/pitt/cs/android/diff/IDiff.java
+++ MsPrj/Updater/src/edu/pitt/cs/android/diff/IDiff.java
@@ -12,14 +12,16 @@ public interface IDiff {
 	 * @param originData
 	 * @param newData
 	 * @return diffData
+	 * @throws Exception 
 	 */
-	public byte[] diff(byte[] originData, byte[] newData);
+	public byte[] diff(byte[] originData, byte[] newData) throws Exception;
 	
 	/**
 	 * @param originData
 	 * @param diffData
 	 * @return newData
+	 * @throws Exception 
 	 */
-	public byte[] merge(byte[] originData, byte[] diffData);
+	public byte[] merge(byte[] originData, byte[] diffData) throws Exception;
 
 }
